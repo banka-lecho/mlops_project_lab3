98af5fe5f32de1667627edbbe5fd56a64d24725f
Shpileva <shpilyova.nastya@mail.ru>
2026-09-09 14:44:30 +0300

    rename image name

--- vault/entrypoint.sh
+++ vault/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# Поднимает dev-сервер Vault и применяет вшитый в образ скрипт инициализации.
 set -e
 
-# Фоновая задача: дождаться готовности API и залить секреты из /seed.sh
-# (сам скрипт скопирован в образ на этапе сборки, см. Dockerfile).
 (
   export VAULT_ADDR=http://127.0.0.1:8200 VAULT_TOKEN=root
   until vault status >/dev/null 2>&1; do sleep 1; done
